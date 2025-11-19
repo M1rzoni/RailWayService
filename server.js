deecaf75ac67d1baf88bad46601fb2e76c5d04b3
Mirza <113027159+M1rzoni@users.noreply.github.com>
2025-11-19 12:16:27 +0100

    first commit

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.post('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`API radi na portu ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`API radi na portu ${PORT}`));
